ab92a7beae6c797a3397958aa952c02bb4c2a525
Zhedong Zheng <Zhedong.Zheng@student.uts.edu.au>
2017-07-26 16:38:01 +1000

    Update train_id_net_res_market_pesudo.m

--- train_id_net_res_market_pesudo.m
+++ train_id_net_res_market_pesudo.m
@@ -3,19 +3,13 @@ function train_id_net_vgg16(varargin)
 % Part 4.1: prepare the data
 % -------------------------------------------------------------------------
 
-% Load character dataset
+% Load the dataset
 imdb = load('./url_data_gan.mat') ;
 imdb = imdb.imdb;
-%imdb.images.set(1:10000) = 3;
 % -------------------------------------------------------------------------
 % Part 4.2: initialize a CNN architecture
 % -------------------------------------------------------------------------
 net = resnet52_market_pesudo();
-
-%netStruct = load('/home/zzd/re_ID_beta23_uts0/data/resnet52_2stream_drop0.9_all/net_single.mat') ;
-%net = dagnn.DagNN.loadobj(netStruct.net) ;
-%net.layers(176).block.loss = 'labelsmooth';
-%net.layers(174).block.rate = 0.75;
 net.conserveMemory = true;
 net.meta.normalization.averageImage = reshape([105.6920,99.1345,97.9152],1,1,3);
 % -------------------------------------------------------------------------
@@ -24,7 +18,7 @@ net.meta.normalization.averageImage = reshape([105.6920,99.1345,97.9152],1,1,3);
 opts.train.averageImage = net.meta.normalization.averageImage;
 opts.train.batchSize = 64;
 opts.train.continue = true; 
-opts.train.gpus = [2,4];
+opts.train.gpus = 2; %gpu id
 opts.train.prefetch = false ;
 opts.train.nesterovUpdate = true ;
 opts.train.expDir = './data/res52_drop0.75_pesudo1_gan36000' ;
@@ -43,7 +37,7 @@ opts.train.numEpochs = numel(opts.train.learningRate) ;
 % --------------------------------------------------------------------
 function inputs = getBatch(imdb,net,batch,opts)
 % --------------------------------------------------------------------
-if(opts.epoch>20)
+if(opts.epoch>20)  % after 20 epoch we start using pesudo label.
     opts.derOutputs = {'objective', 0,'objective_pesudo',1} ;
 end
 im_url = imdb.images.data(batch) ; 
